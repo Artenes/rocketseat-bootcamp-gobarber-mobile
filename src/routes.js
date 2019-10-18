ec59749b7e084dd688f44abd0863823dc2ba8061
Artenes Nogueira <artenesama@gmail.com>
2019-10-17 21:22:56 -0400

    Applys style to tabs and Profile and Dashboard pages

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
@@ -15,7 +16,19 @@ export default (isSigned = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({ Dashboard }),
+        App: createBottomTabNavigator(
+          { Dashboard, Profile },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
