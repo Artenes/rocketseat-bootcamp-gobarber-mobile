f6b1d4f09aec3e2e7ae2ff713287fcc9c9740bc9
Artenes Nogueira <artenesama@gmail.com>
2019-10-19 20:47:34 -0400

    Shows date time picker for android and ios

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
+import { Container } from '~/pages/Profile/styles';
+
 // import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
 
-SelectDateTime.navigationOptions = {
+SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Select the time',
-};
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}>
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
